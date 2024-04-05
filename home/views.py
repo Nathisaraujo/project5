1469baeb5853903245ad198bb87d97fd1da1a38f
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-05 10:22:21 +0000

    added home app and templates

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_page(request):
+    """ A view to return the home page """
+    
+    return render(request, 'home/home_page.html')
