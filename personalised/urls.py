40dd776f334d55b7675c3b92605fab26693835a3
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-16 19:05:37 +0000

    deleted all models in personalised app and also the admin, views, urls and forms files to start over again

--- personalised/urls.py
+++ personalised/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.personalised_page, name='personalised'),
-    # path('order/', views.order_page, name='order'),
-    path('personalised/order/', views.personalised_order, name='personalised_order'),
+    # path('product/<int:product_id>/', views.personalised_product_detail, name='personalised_product_detail'),
+    # path('process_order/', views.process_order, name='process_order'),
 ] 
