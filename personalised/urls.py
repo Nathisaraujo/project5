d701715d6da4372d68bb4679afdc9a711fe007fa
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-17 15:50:53 +0000

    added personalised model

--- personalised/urls.py
+++ personalised/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.personalised_page, name='personalised'),
-    # path('product/<int:product_id>/', views.personalised_product_detail, name='personalised_product_detail'),
-    # path('process_order/', views.process_order, name='process_order'),
 ] 
