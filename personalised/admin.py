81b5a35d0b34c51d85d8f0a358dbf982c57fe200
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-20 12:21:26 +0000

    deleted personalised app - won't have in the project anymore

--- personalised/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Personalised
-
-
-# Register your models here.
-
-admin.site.register(Personalised)
--- personalised/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import PersonalisedOrder
-
-urlpatterns = [
-    path('', views.personalised_page, name='personalised'),
-    path('order/', views.PersonalisedOrder, name='personalised_order'),
-    path('order-summary/', views.order_summary, name='order_summary'),
-] 
