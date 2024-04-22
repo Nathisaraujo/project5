f0e605ed9f676806b833c4dc8ef5da0c95c47478
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-22 14:42:38 +0000

    added community app and set up view and template

---
+++ community/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def community_page(request):
+    """ A view to return the home page """
+    
+    return render(request, 'community/community_page.html')
--- drawing_gratitude/urls.py
+++ drawing_gratitude/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('about/', include('about.urls')),
     path('profile/', include('profiles.urls')),
+    path('community/', include('community.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
