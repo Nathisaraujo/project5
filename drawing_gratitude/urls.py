10bfa525a7a368fed2a2f2fd8ad116d4e163502d
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-25 18:11:21 +0000

    created blog app and set up views, urls and templates

--- drawing_gratitude/urls.py
+++ drawing_gratitude/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('profile/', include('profiles.urls')),
     path('community/', include('community.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
