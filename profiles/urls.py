24144e8f7dd250705e6b89bc778e80aff753a30b
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-04-26 18:26:19 +0000

    changed home page style, added community page buttons, fixed aligment

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('recommend/', views.recommend, name='recommend'),
-    path('recommendation/success/', views.recommendation_success, name='recommendation_success'),
     path('update_profile/', views.update_profile, name='update_profile'),
     path('orders/', views.orders, name='orders'),
 ]
