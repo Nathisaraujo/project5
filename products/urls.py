80a81bfb2ecbdf981a822527eac6e476cdbcc7d2
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-05-09 13:12:03 +0000

    product admin - added functionality to delete products

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('add_tags/', views.add_product_tags, name='add_product_tags'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ] 
--- products/views.py
+++ products/views.py
@@ -112,9 +112,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -131,9 +131,9 @@ def add_product_tags(request):
     if request.method == 'POST':
         form = ProductTagsForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product tags!')
-            return redirect(reverse('add_product_tags'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product tags. Please ensure the form is valid.')
     else:
@@ -174,4 +174,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
-    
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
