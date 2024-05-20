17537aed115f48fb86f91130afb397816f945652
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-05-20 18:18:23 +0000

    tidying up - bag app - pep8

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,6 @@
-from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from decimal import Decimal
 from products.models import Product
 
 
@@ -14,12 +14,12 @@ def bag_contents(request):
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            if product.offers:  # Assuming 'offers' indicates discounted products
-                discounted_price = product.price * (1 - Decimal('0.15'))  # Assuming a 15% discount
+            if product.offers:
+                discounted_price = product.price * (1 - Decimal('0.15'))
                 total_discounted_price += discounted_price * item_data
             else:
                 discounted_price = product.price
-            total += discounted_price * item_data  # Include discounted price in the total
+            total += discounted_price * item_data
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -28,7 +28,11 @@ def bag_contents(request):
                 'discounted_price': discounted_price,
             })
 
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100) if total < settings.FREE_DELIVERY_THRESHOLD else 0
+    delivery = (
+        total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        if total < settings.FREE_DELIVERY_THRESHOLD
+        else 0
+    )
     grand_total = total + delivery
 
     context = {
@@ -37,7 +41,9 @@ def bag_contents(request):
         'total_discounted_price': total_discounted_price,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_delta': max(0, settings.FREE_DELIVERY_THRESHOLD - total),
+        'free_delivery_delta': (
+            max(0, settings.FREE_DELIVERY_THRESHOLD - total)
+        ),
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
