9034310e122a462100452a6a76a23e5df3164fef
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-05-22 21:24:27 +0000

    fixed save events bug, deleted timer, added people interested counter and option to login to save event

--- events/urls.py
+++ events/urls.py
@@ -4,17 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('save_event/<int:event_id>/', views.save_event, name='save_event'),
-    path('unsave/<int:event_id>/', views.unsave_event, name='unsave_event'),
-    path(
-        'unsave_profile/<int:event_id>/',
-        views.unsave_event_profile,
-        name='unsave_event_profile'
-    ),
+    path('unsave_event/<int:event_id>/', views.unsave_event, name='unsave_event'),
     path('add_event/', views.add_event, name='add_event'),
     path('edit_event/<int:event_id>/', views.edit_event, name='edit_event'),
-    path(
-        'delete_event/<int:event_id>/',
-        views.delete_event,
-        name='delete_event'
-    ),
+    path('delete_event/<int:event_id>/', views.delete_event, name='delete_event'),
+    path('unsave_event_profile/<int:event_id>/', views.unsave_event_profile, name='unsave_event_profile'),
 ]
+
--- events/views.py
+++ events/views.py
@@ -1,39 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core import serializers
-from django.core.mail import send_mail
-from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.urls import reverse
 from django.utils import timezone
 from datetime import datetime
-from profiles.models import UserProfile
 from .forms import EventForm
 from .models import Event
 
 
 def event_list(request):
-    """
-    Display a list of events, including those saved by the user.
-
-    Retrieves all events from the database and determines if each event
-    has been saved by the authenticated user. Generates JSON data for the
-    events. Passes the events data to the template for rendering.
-    """
     events = Event.objects.all()
     now = datetime.now()
+    saved_event_ids = []
 
     if request.user.is_authenticated:
-        saved_event_ids = Event.objects.filter(
-            save_event=request.user
-        ).values_list(
-            'id', flat=True
-        )
-    else:
-        saved_event_ids = []
-    events_json = serializers.serialize('json', events)
+        saved_event_ids = request.user.saved_events.values_list('id', flat=True)
 
     context = {
-        'events_json': events_json,
         'events': events,
         'saved_event_ids': saved_event_ids,
         'now': now,
@@ -44,68 +27,21 @@ def event_list(request):
 
 @login_required
 def save_event(request, event_id):
-    """
-    Save an event to the user's saved events list.
-
-    Fetches the event by its ID and adds the current user to the list of users
-    who have saved the event. Sends a reminder email if the event is happening
-    tomorrow. Redirects to the event list page.
-    """
     event = get_object_or_404(Event, pk=event_id)
-    tomorrow = timezone.now() + timezone.timedelta(days=1)
     event.save_event.add(request.user)
-    event.save()
-    send_event_reminders(event)
-
-    if event.date_and_time.date() == tomorrow.date():
-        messages.info(
-            request,
-            f'{event.title} added to your events. '
-            'A reminder email will be sent one day before the event.'
-        )
-    else:
-        messages.info(request, f'{event.title} added to your events.')
+    messages.info(request, f'{event.title} added to your events.')
     return redirect('event_list')
-
-
-def send_event_reminders(event):
-    tomorrow = timezone.now() + timezone.timedelta(days=1)
-
-    if event.date_and_time.date() == tomorrow.date():
-        for user in event.save_event.all():
-            profile = UserProfile.objects.get(user=user)
-            send_mail(
-                'Drawing Gratitude - Event Reminder',
-                f"Don't forget, '{event.title}' is happening tomorrow!",
-                'drawinggratitude@email.com',
-                [profile.user.email],
-                fail_silently=False,
-            )
-
 
 @login_required
 def unsave_event(request, event_id):
-    """
-    Remove an event from the user's saved events list.
-
-    Fetches the event by its ID and removes the current user from the list
-    of users who have saved the event. Redirects to the event list page.
-    """
     event = get_object_or_404(Event, pk=event_id)
     event.save_event.remove(request.user)
-    event.save()
     messages.info(request, f'Event "{event.title}" removed from saved events')
     return redirect('event_list')
 
 
 @login_required
 def unsave_event_profile(request, event_id):
-    """
-    Remove an event from the user's saved events list from the profile page.
-
-    Fetches the event by its ID and removes the current user from the list
-    of users who have saved the event. Redirects to the saved events page.
-    """
     event = get_object_or_404(Event, pk=event_id)
     event.save_event.remove(request.user)
     event.save()
@@ -115,13 +51,6 @@ def unsave_event_profile(request, event_id):
 
 @login_required
 def add_event(request):
-    """
-    Add a new event.
-
-    Allows store owners to add new events. Validates the form data and saves
-    the event to the database. Redirects to the event list page upon success.
-
-    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -156,13 +85,6 @@ def add_event(request):
 
 @login_required
 def edit_event(request, event_id):
-    """
-    Edit an existing event.
-
-    Allows store owners to edit existing events. Validates the form data and
-    updates the event in the database. Redirects to the event list page upon
-    success.
-    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -194,14 +116,6 @@ def edit_event(request, event_id):
 
 @login_required
 def delete_event(request, event_id):
-    """
-    Delete an event.
-
-    Allows only admins to delete events. Fetches the event
-    by its ID and deletes it from the database. Redirects to the event list
-    page upon successful deletion.
-
-    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
