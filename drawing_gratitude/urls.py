a5a6571b966c69df1e545bbc73d82d424c55df61
Nathalia de Araujo Silva <nat.advogada@gmail.com>
2024-05-28 09:56:18 +0000

    added a 404 page

--- drawing_gratitude/urls.py
+++ drawing_gratitude/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -33,3 +34,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('events/', include('events.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'drawing_gratitude.views.handler404'
